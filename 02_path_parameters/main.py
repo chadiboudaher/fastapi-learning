d755dbeada102d8d9b0e4ce492d81759e0628b15
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-03 21:15:34 +0300

    Path inside a path

--- 02_path_parameters/main.py
+++ 02_path_parameters/main.py
@@ -1,4 +1,10 @@
+from enum import Enum
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
@@ -9,12 +15,39 @@ app = FastAPI()
 # 1. Pass a path and return the id in a message
 # 2. Identify the type of parameter set
 
-@app.get("/users/me")
-async def read_user_me():
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {
+#         "user_id": "The current user"
+#     }
+
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: int):
+#     return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {
+            "model_name": model_name,
+            "message": "Deep Learning FTW!",
+        }
+    
+    if model_name.value == "lenet":
+        return {
+            "model_name": model_name,
+            "message": "LeCNN all the images"
+        }
+    
     return {
-        "user_id": "The current user"
+        "model_name": model_name,
+        "message": "Have some residual"
     }
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: int):
-    return {"user_id": user_id}
+
+# Path inside a path
+# /files/{file_path:path}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}

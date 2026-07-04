39318b41d9cad8dac777c58c59fa2807d506cc55
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-04 15:46:07 +0300

    Create custom Validator

--- 05_query_parameters_and_string_validations/main.py
+++ 05_query_parameters_and_string_validations/main.py
@@ -1,7 +1,15 @@
+import random
 from typing import Annotated
 from fastapi import FastAPI, Query
+from pydantic import AfterValidator
 
 app = FastAPI()
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
 
 @app.get("/")
 async def root():
@@ -20,17 +28,15 @@ async def root():
 
 # we can add additional validations
 
+def chack_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
 @app.get("/items/")
-async def read_items(
-    hidden_query: Annotated[str | None, Query(
-        include_in_schema=False
-        )] = None):
-    # results = {"items": [
-    #     {"item_id": "Foo"},
-    #     {"item_id": "Bar"}
-    # ]}
-    if hidden_query:
-        return {"hidden_query": hidden_query}
-    
+async def read_items(id: Annotated[str | None, AfterValidator(chack_valid_id)] = None):
+    if id:
+        item = data.get(id)
     else:
-        return {"hidden_query": "Not Found"}
+        id, item = random.choice(list(data.item()))
+    return {"id": id, "name": item}

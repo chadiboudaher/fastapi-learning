0b480c615798dd0f159c1be36efda45e765c5aca
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-05 10:00:58 +0300

    Adding multiple models in body

--- 08_multiple_parameters/main.py
+++ 08_multiple_parameters/main.py
@@ -10,19 +10,24 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello"}
 
 @app.put("/items/{item_id}") # Update
 async def update_item(
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
-    q: str | None = None,
-    item: Item | None = None
+    item_id: int,
+    item: Item,
+    user: User
 ):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item: 
-        results.update({"item": item})
+    results = {
+        "item_id": item_id,
+        "user": user,
+        "item": item
+        }
     return results

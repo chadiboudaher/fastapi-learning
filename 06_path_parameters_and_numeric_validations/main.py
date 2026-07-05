bbd101c4028ae188afb86e10a199ce96a7a9928d
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-05 09:13:25 +0300

    Declare a ge or le and so on

--- 06_path_parameters_and_numeric_validations/main.py
+++ 06_path_parameters_and_numeric_validations/main.py
@@ -9,8 +9,8 @@ async def root():
 
 @app.get("/items/{item_id}")
 async def read_items(
-    item_id: Annotated[int, Path(title="The ID of the item to get")],
-    q: Annotated[str | None, Query(alias="item-query")] = None
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)],
+    q: str
 ):
     results = {"item_id": item_id}
     if q:

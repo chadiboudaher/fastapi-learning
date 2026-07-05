403e888182ccd0c09b1488a9414de01367193ff3
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-05 09:22:55 +0300

    Summarize lesson in REAME

---
+++ 06_path_parameters_and_numeric_validations/README.md
@@ -0,0 +1,13 @@
+# Path Parameters and Numeric Validations
+
+`Path` was introduced in this lesson.
+
+1. it is located in the main part of the `URL`.
+2. It is always mandatory.
+
+We can declare numeric validations for both `Path` and `Query`:
+
+- gt = greater than.
+- ge = greater than or equal.
+- lt = less than.
+- le = less than or equal
--- 06_path_parameters_and_numeric_validations/main.py
+++ 06_path_parameters_and_numeric_validations/main.py
@@ -9,10 +9,13 @@ async def root():
 
 @app.get("/items/{item_id}")
 async def read_items(
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)],
-    q: str
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1, le=1000)],
+    q: str,
+    size: Annotated[float, Query(gt=0, lt=10.5)]
 ):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
+    if size:
+        results.update({"size": size})
     return results

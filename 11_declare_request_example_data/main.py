0bc4f4094b2fd5e905ad8b09cfcbca2882b761cf
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-06 15:32:18 +0300

    Place example inside Body

--- 11_declare_request_example_data/main.py
+++ 11_declare_request_example_data/main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Item(BaseModel):
-    name: str = Field(examples=["Foo", "Boo"])
-    description: str | None = Field(default=None, examples=["A very nice item", "A very shitty item"])
-    price: float = Field(examples=[12.3, 12.9])
-    tax: float | None = Field(default=None, examples=[3.2, 4.5])
+    # name: str = Field(examples=["Foo", "Boo"])
+    # description: str | None = Field(default=None, examples=["A very nice item", "A very shitty item"])
+    # price: float = Field(examples=[12.3, 12.9])
+    # tax: float | None = Field(default=None, examples=[3.2, 4.5])
+
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
     # model_config = {
     #     "json_schema_extra": {
@@ -29,6 +35,11 @@ async def root():
     }
 
 @app.put("/item/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Annotated[Item, Body(examples=[{
+    "name": "Fool",
+    "description": "very Interesting Item",
+    "price": 35.4,
+    "tax": 3.2
+}])]):
     results= {"item_id": item_id, "item": item}
     return results

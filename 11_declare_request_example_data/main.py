2d532ea424ac41b4de7a51934e4b1591cc736cae
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-06 15:25:54 +0300

    Use Field to put body examples

--- 11_declare_request_example_data/main.py
+++ 11_declare_request_example_data/main.py
@@ -1,26 +1,26 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+    name: str = Field(examples=["Foo", "Boo"])
+    description: str | None = Field(default=None, examples=["A very nice item", "A very shitty item"])
+    price: float = Field(examples=[12.3, 12.9])
+    tax: float | None = Field(default=None, examples=[3.2, 4.5])
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2
-                }
-            ]
-        }
-    }
+    # model_config = {
+    #     "json_schema_extra": {
+    #         "examples": [
+    #             {
+    #                 "name": "Foo",
+    #                 "description": "A very nice Item",
+    #                 "price": 35.4,
+    #                 "tax": 3.2
+    #             }
+    #         ]
+    #     }
+    # }
 
 @app.get("/")
 async def root():

dafc1218b31c3646651abde2b5a9b80f2e08bfb0
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-06 14:57:45 +0300

    Checking pure list bodies

--- 10_body_nested_models/main.py
+++ 10_body_nested_models/main.py
@@ -31,3 +31,7 @@ async def root():
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+    return images

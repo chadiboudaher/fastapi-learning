06f6e80ab835f37c6274458790809c71df411e41
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-07 15:13:14 +0300

    Summarize lesson in README

---
+++ 15_response_model_return_type/README.md
@@ -0,0 +1,11 @@
+# Response Model
+
+## What is Response Model?
+
+`response_model` defines the shape, type, and structure of data your API returns. It acts as a filter, validator, and documentation generator.
+
+## Why use it?
+
+1. Security -> Hide sensitive fields
+2. Validation -> Ensures data matches expected format
+3. Conversion -> auto-convert type
--- 15_response_model_return_type/main.py
+++ 15_response_model_return_type/main.py
@@ -5,36 +5,34 @@ from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-# class Item(BaseModel):
-#     name: str
-#     description: str | None = None
-#     price: float
-#     tax: float | None = None
-#     tags: list[str] = []
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float = 10.5
+    tags: list[str] = []
 
-class BaseUser(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
-
-class UserIn(BaseUser):
-    password: str
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {
+        "name": "Baz", 
+        "description": None, 
+        "price": 50.2, 
+        "tax": 10.5, 
+        "tags": []
+    },
+}
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
 
-# @app.post("/items/", response_model=Item)
-# async def create_item(item: Item) -> Item:
-#     return item
 
-# @app.get("/items/", response_model=list[Item])
-# async def read_items() -> Any:
-#     return [
-#         Item(name="Portal Gun", price=42.0),
-#         Item(name="Plumbus", price=32.0)
-#     ]
+@app.get("/items/{item_id}", response_model=Item, response_model_include={"name", "description"})
+async def read_item(item_id: str):
+    return items[item_id]
 
-@app.post("/user/")
-async def create_user(user: UserIn) -> BaseUser:
-    return user
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+async def read_item_public_data(item_id: str):
+    return items[item_id]

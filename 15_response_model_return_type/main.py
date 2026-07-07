bdc97c39d6fd323aba260a715f979522293d5e75
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-07 14:49:03 +0300

    Use inheritance

--- 15_response_model_return_type/main.py
+++ 15_response_model_return_type/main.py
@@ -5,39 +5,36 @@ from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: list[str] = []
 
-class UserIn(BaseModel):
+class BaseUser(BaseModel):
     username: str
+    email: EmailStr
+    full_name: str | None = None
+
+class UserIn(BaseUser):
     password: str
-    email: EmailStr
-    full_name: str | None = None
-
-class UserOut(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
 
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item) -> Item:
-    return item
+# @app.post("/items/", response_model=Item)
+# async def create_item(item: Item) -> Item:
+#     return item
 
-@app.get("/items/", response_model=list[Item])
-async def read_items() -> Any:
-    return [
-        Item(name="Portal Gun", price=42.0),
-        Item(name="Plumbus", price=32.0)
-    ]
+# @app.get("/items/", response_model=list[Item])
+# async def read_items() -> Any:
+#     return [
+#         Item(name="Portal Gun", price=42.0),
+#         Item(name="Plumbus", price=32.0)
+#     ]
 
-@app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn) -> Any:
+@app.post("/user/")
+async def create_user(user: UserIn) -> BaseUser:
     return user

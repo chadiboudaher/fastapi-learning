b1744be168cf660d53a9ecfc050d24f01d1d0359
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-09 16:41:45 +0300

    Use deprecated on operation

--- 21_path_operation_config/main.py
+++ 21_path_operation_config/main.py
@@ -10,11 +10,15 @@ class Item(BaseModel):
     tax: float | None = None
     tags: set[str] = set()
 
-@app.post("/items/", status_code=status.HTTP_201_CREATED, tags=["items"])
+@app.post("/items/", 
+          status_code=status.HTTP_201_CREATED, 
+          tags=["items"],
+          summary="Create an item hoe",
+          description="Create a item with al the information, name, desc, price, tax")
 async def create_item(item: Item) -> Item:
     return item
 
-@app.get("/items/", tags=['items'])
+@app.get("/items/", tags=['items'], deprecated=True)
 async def read_items():
     return [{
         "name": "Foo",

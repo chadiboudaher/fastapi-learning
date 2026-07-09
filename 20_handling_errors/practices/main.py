253db7d856bec11de05810bee61e2ce952df631c
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-09 15:44:26 +0300

    Create custom error handler

--- 20_handling_errors/practices/main.py
+++ 20_handling_errors/practices/main.py
@@ -6,6 +6,32 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+@app.exception_handler(RequestValidationError)
+async def validation_handler(requst: Request,
+                             exc: RequestValidationError):
+    errors = []
+    for error in exc.errors():
+        field = error["loc"][-1]
+        msg = error["msg"]
+        errors.append(f"{field}: {msg}")
+
+    return JSONResponse(
+        status_code=422,
+        content={
+            "error": "Validation Field",
+            "details": errors
+        }
+    )
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request,
+                                 exc: HTTPException):
+    return JSONResponse(status_code=exc.status_code,
+                        content={
+                            "error": exc.detail,
+                            "status": exc.status_code
+                        })
+
 class User(BaseModel):
     name: str
     age: int
@@ -38,6 +64,15 @@ async def get_user(user_id: int):
 
 @app.post("/users/")
 async def create_user(user: User):
+
+    for existing in users_db.values():
+        if existing["name"] == user.name:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"User {user.name} already exists")
+
+    if user.age  < 0 or user.age > 120:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Age must bebetween 0 and 120")
     id = max(users_db.keys()) + 1
     users_db[id] = user.dict()
     return {

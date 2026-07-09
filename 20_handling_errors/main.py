08f78211b10fe379fc6e4ccbe9cbcff80ba98526
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-09 12:21:38 +0300

    Handling errors

--- 20_handling_errors/main.py
+++ 20_handling_errors/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, HTTPException 
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
-from fastapi.responses import PlainTextResponse
-from starlette.exceptions import HTTPException as ScarletteHTTPException
+from fastapi.responses import JSONResponse
+# from starlette.exceptions import HTTPException as ScarletteHTTPException
+from pydantic import BaseModel
 
 """
 HTTPException - For raising HTTP errors in your routes
@@ -12,22 +14,43 @@ ScarletteHTTPException - Starlette's base HTTP exception (parent of FastAPI's)
 
 app = FastAPI()
 
-@app.exception_handler(ScarletteHTTPException)
-async def http_exception_handler(request, exc):
-    return PlainTextResponse(str(exc.detail), 
-                             status_code=exc.status_code)
-
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request,
+async def validation_exception_handler(request: Request,
                                        exc: RequestValidationError):
-    message = "Validation Erros:"
-    for error in exc.errors():
-        message += f"\nField: {error['loc']}, Error: {error['msg']}"
-    return PlainTextResponse(message, status_code=400)
+    return JSONResponse(
+        status_code=422,
+        content=jsonable_encoder({
+            "detail": exc.errors(),
+            "body": exc.body
+        })
+    )
 
-@app.get("/items/{item_id}")
-async def read(item_id: int):
-    if item_id == 3:
-        raise HTTPException(status_code=418,
-                            detail="Nope! I don't like 3.")
-    return {"item_id": item_id}
+class Item(BaseModel):
+    title: str
+    size: int
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+    
+
+# @app.exception_handler(ScarletteHTTPException)
+# async def http_exception_handler(request, exc):
+#     return PlainTextResponse(str(exc.detail), 
+#                              status_code=exc.status_code)
+
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request,
+#                                        exc: RequestValidationError):
+#     message = "Validation Erros:"
+#     for error in exc.errors():
+#         message += f"\nField: {error['loc']}, Error: {error['msg']}"
+#     return PlainTextResponse(message, status_code=400)
+
+# @app.get("/items/{item_id}")
+# async def read(item_id: int):
+#     if item_id == 3:
+#         raise HTTPException(status_code=418,
+#                             detail="Nope! I don't like 3.")
+#     return {"item_id": item_id}

a49b9818f7ce515a17d3dc7b402cee80e75e0e3a
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-10 20:55:20 +0300

    Create engine and connect

--- sqlalchemy/01_sqlalchemy_intro/main.py
+++ sqlalchemy/01_sqlalchemy_intro/main.py
@@ -1,25 +1,6 @@
-"""
-**engine** act as a source of connections to a particular database.
+# We create an engine, which we use to connect to a database.
+from sqlalchemy import create_engine
 
-sqlite+pysqlite:///:memory:
-this tell us three things:
-1. what kind of data are we communicating with.
-2. DBAPI - a third party driver used to interact with a particular databases (pysqlite for SQLite).
-3. memory -> meaning we are using in-memory-only database.
-"""
+engine = create_engine('sqlite:///mydatabase.db', echo=True)
 
-from sqlalchemy import create_engine, text
-
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-
-# with engine.connect() as conn:
-#     result = conn.execute(text("select 'hello world'"))
-#     print(result.all()python)
-
-with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 1, "y": 1}, {"x": 2, "y": 4}]
-    )
-    conn.commit()
+conn = engine.connect()

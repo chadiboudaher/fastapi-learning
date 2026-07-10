ea8263f520c06dfaca044e717c0c4bfbebb04ff1
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-10 15:00:11 +0300

    Implement dependencies

--- 23_dependencies/README.md
+++ 23_dependencies/README.md
@@ -3,3 +3,14 @@
 ## What is `Dependency Injection`?
 
 It means that there are ways for your code to declare things things that it require to work and use: `dependencies`.
+
+## What dependencies do?
+
+They are reusable components that can be injected into your path operations. They handle common tasks like:
+
+- Authentication/Authorization.
+- Database sessions.
+- Logging & monitoring
+- Rate limiting
+- caching
+- COnfiguring loading
--- 23_dependencies/main.py
+++ 23_dependencies/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "hello"}
+
+async def common_parameters(q: str | None = None,
+                            skip: int = 0,
+                            limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+commonsDep = Annotated[dict, Depends(common_parameters)]
+
+@app.get("/items/")
+async def read_items(
+    commons: Annotated[dict, commonsDep]):
+    return commons
+
+@app.get("/users/")
+async def read_users(commons: Annotated[dict, commonsDep]):
+    return commons

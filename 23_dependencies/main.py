61b768ed1b300006a11a93a53baaeeb130e869f7
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-10 15:30:55 +0300

    Adding dependencies with classes

--- 23_dependencies/main.py
+++ 23_dependencies/main.py
@@ -3,22 +3,28 @@ from fastapi import FastAPI, Depends
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "hello"}
+fake_items_db = [
+    {"item_name": "Foo"}, 
+    {"item_name": "Bar"}, 
+    {"item_name": "Baz"}
+]
 
-async def common_parameters(q: str | None = None,
-                            skip: int = 0,
-                            limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
 
-commonsDep = Annotated[dict, Depends(common_parameters)]
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0,
+                 limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
 
 @app.get("/items/")
-async def read_items(
-    commons: Annotated[dict, commonsDep]):
-    return commons
-
-@app.get("/users/")
-async def read_users(commons: Annotated[dict, commonsDep]):
-    return commons
+async def read_items(commons: 
+                     Annotated[CommonQueryParams, 
+                               Depends(CommonQueryParams)]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response

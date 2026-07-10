7c60c568b548b6a10ed054cc2d661b8dfb0d75a3
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-10 21:05:08 +0300

    Create table and insert values

--- sqlalchemy/01_sqlalchemy_intro/main.py
+++ sqlalchemy/01_sqlalchemy_intro/main.py
@@ -1,6 +1,18 @@
 # We create an engine, which we use to connect to a database.
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 engine = create_engine('sqlite:///mydatabase.db', echo=True)
 
 conn = engine.connect()
+
+
+conn.execute(text("CREATE TABLE IF NOT EXISTS people (name str, age int)"))
+
+# To persist the changes
+conn.commit()
+
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+session.execute(text('INSERT INTO PEOPLE (name, age) VALUES ("Mike", 30)'))
+session.commit()

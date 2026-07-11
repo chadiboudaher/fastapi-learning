2faf629ceba5f951990fbd774d145c1f97365a8f
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-11 14:14:49 +0300

    Intro to security

--- sqlalchemy/01_sqlalchemy_intro/main.py
+++ sqlalchemy/01_sqlalchemy_intro/main.py
@@ -1,5 +1,5 @@
 # We create an engine, which we use to connect to a database.
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, insert, Float, ForeignKey
 
 engine = create_engine('sqlite:///mydatabase.db', echo=True)
 
@@ -13,10 +13,25 @@ people = Table(
     Column("age", Integer)
 )
 
+things = Table(
+    "things",
+    meta,
+    Column("id", Integer, primary_key=True),
+    Column("description", String, nullable=False),
+    Column("value", Float),
+    Column("owner", Integer, ForeignKey("people.id"))
+)
+
 meta.create_all(engine)
 
 conn = engine.connect()
 
-insert_statement = people.insert().values(name="Mike", age=30)
-result = conn.execute(insert_statement)
-conn.commit()
+# insert_statement = people.insert().values(name="Mike", age=30)
+# result = conn.execute(insert_statement)
+# conn.commit()
+
+# select_st = people.select().where(people.c.age > 30)
+# result = conn.execute(select_st)
+
+# for row in result.fetchall():
+#     print(row)

77529798e7ae4beda9376284c32004b630eb9524
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-11 18:00:45 +0300

    Create UserCreate and UserOut

--- projects/notes-api/models.py
+++ projects/notes-api/models.py
@@ -7,3 +7,12 @@ class UserBase(BaseModel):
     username: str
     email: EmailStr
     full_name: Optional[str] = None
+
+# UserCreate - it is the addition of password to userbase 
+class UserCreate(UserBase):
+    password: str
+
+# UserOut -
+class UserOut(UserBase):
+    id: int
+    created_at: datetime

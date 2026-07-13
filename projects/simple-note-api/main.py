3d4dd494f80cf9a3632a91fda5f98da890da56f5
chadiboudaher <chadiboudaher7@gmail.com>
2026-07-13 16:41:28 +0300

    Create get note operation

--- projects/simple-note-api/main.py
+++ projects/simple-note-api/main.py
@@ -18,3 +18,18 @@ async def create_note(note: schemas.NoteCreate,
                       user_id: int,
                       conn: Connection = Depends(get_conn)):
     return crud.create_note(conn, note, user_id)
+
+@app.get("/notes/", response_model=List[schemas.NoteOut])
+async def read_notes(skip: int = 0, 
+                    limit: int = 100, 
+                    conn: Connection = Depends(get_conn)):
+    return crud.get_notes(conn, skip, limit)
+
+@app.get("/notes/{note_id}", response_model=schemas.NoteOut)
+async def read_note(note_id: int,
+                    conn: Connection = Depends(get_conn)):
+    db_note = crud.get_note(conn, note_id)
+    if db_note is None:
+        raise HTTPException(status_code=404,
+                            detail="Note not found")
+    return db_note
